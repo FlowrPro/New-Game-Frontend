483881b65c76b8cc59e8aaa2ad8c689139dca02a
CharmedZ <fiestaadam10@gmail.com>
2025-11-19 17:02:22 -0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const GAME_CONSTANTS = {
+  baseXP: 50,
+  xpGrowthRate: 2,
+  baseHealth: 50,
+  healthPerLevel: 200,
+  baseDamage: 10,
+  damagePerLevel: 30,
+  baseSpeed: 2,
+  maxSlots: 6,
+  goldPerHourDefault: 10,
+  ui: {
+    slotSize: 40,
+    slotGap: 10,
+    slotStartX: 10,
+    slotStartY: 100,
+  },
+  cooldowns: {
+    default: 1000,
+    long: 5000,
+    ultra: 10000,
+  },
+};
